df67fb5ada1531d843c939fd4321470537f28727
Denis Catruna <devengvengg@gmail.com>
2023-01-18 08:50:30 +0100

    Migrations Fixes Create/Edit/Delete DashBoard

--- src/components/react-admin/migrations.js
+++ src/components/react-admin/migrations.js
@@ -1,24 +1,61 @@
 import { useMediaQuery } from '@mui/material';
-import { List, SimpleList, Datagrid, TextField } from 'react-admin';
+// Migrations Inport:
+import { List, SimpleList, Datagrid, TextField, EditButton, Edit, Create, SimpleForm, TextInput } from 'react-admin';
+import { useRecordContext} from 'react-admin';
+
+// Migrations:
+const postFilters = [
+    <TextInput source="q" label="Search" alwaysOn />
+];
 
 export const MigrationList = () => {
     const isSmall = useMediaQuery((theme) => theme.breakpoints.down('sm'));
     // JSX:
     return (
-    <List>
+        <List filters={postFilters}>
         {isSmall ? (
-        <SimpleList
+            <SimpleList
             primaryText={(record) => record.id}
             secondaryText={(record) => record.migration}
             tertiaryText={(record) => record.batch}
-        />
+            >
+            <EditButton />
+            </SimpleList>
         ) : (
-        <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <TextField source="migration" />
-            <TextField source="batch" />
-        </Datagrid>
+            <Datagrid rowClick="edit">
+                <TextField source="id" />
+                <TextField source="migration" />
+                <TextField source="batch" />
+                <EditButton />
+            </Datagrid>
         )}
-    </List>
+        </List>
     );
 };
+
+// Migrations Create
+const MigrationTitle = () => {
+    const record = useRecordContext();
+    // JSX:
+    return <span>Migration {record ? `"${record.migration}"` : ''}</span>;
+};
+
+export const MigrationEdit = () => (
+    <Edit title={<MigrationTitle />}>
+        <SimpleForm>
+            <TextInput source="id" disabled />
+            <TextInput source="migration" />
+            <TextInput source="batch" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const MigrationCreate = () => (
+    <Create>
+        <SimpleForm>
+        <TextInput source="id" disabled />
+        <TextInput source="migration" />
+        <TextInput source="batch" />
+        </SimpleForm>
+    </Create>
+);
