9ede2f97040d9f8e684602237e4772c498e57856
Denis Catruna <devengvengg@gmail.com>
2023-02-01 10:01:49 +0100

    Autorizacion Tokens Frontend

--- src/components/react-admin/admin.js
+++ src/components/react-admin/admin.js
@@ -15,26 +15,41 @@ import CustomerIcon from '@mui/icons-material/SupportAgent';
 // API Externa:
 import { EcomapsList} from 'components/react-admin/ecomaps';
 import EcomapsIcon from '@mui/icons-material/Palette';
-
+// AuthProvider Token:
+import { default as AuthProvider } from 'components/react-admin/authProvider';
+import { default as Login } from 'pages/login'
+import { useState } from 'react';
 // API Proveedora:
 //const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 //const dataProvider = jsonServerProvider('http://proyecto8.test/api/records');
 const dataProvider = jsonapiClient('http://proyecto8.test/api');
 
 // Recursos:
-const RAdmin = () => (
-  <Admin
-    basename="/dashboard"
-    dataProvider={dataProvider}
-    layout={AdminLayout}
-  >
-    <Resource name="ecomaps" list={EcomapsList} icon={EcomapsIcon} />
-    <Resource name="customers" list={CustomerList} icon={CustomerIcon} edit={CustomerEdit} create={CustomerCreate} />
-    {/* <Resource name="migrations" 
-      list={MigrationList} icon={MigrationIcon} edit={MigrationEdit} create={MigrationCreate}/>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} /> */}
-    <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name" />
-  </Admin>
-)
+const RAdmin = () => {
+  // Enviar Token en Cada Request al Backend]
+  function handleDataProvider(dataProvider) {
+    setDataProvider(() => dataProvider)
+  }
+  const myLogin = <Login handleDataProvider={handleDataProvider} />
+  const API_URL = `${process.env.REACT_APP_BACKEND_URL}/api`
+  const [dataProvider, setDataProvider] = useState(null)
+  if (!dataProvider) handleDataProvider(jsonapiClient(API_URL))
+  // JSX:
+  return (
+    <Admin
+      basename="/dashboard"
+      dataProvider={dataProvider}
+      authProvider={AuthProvider}
+      loginPage={myLogin}
+    >
+      <Resource name="ecomaps" list={EcomapsList} icon={EcomapsIcon} />
+      <Resource name="customers" list={CustomerList} icon={CustomerIcon} edit={CustomerEdit} create={CustomerCreate} />
+      {/* <Resource name="migrations" 
+        list={MigrationList} icon={MigrationIcon} edit={MigrationEdit} create={MigrationCreate}/>
+      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} /> */}
+      <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name" />
+    </Admin>
+  )
+}
 
 export default RAdmin;
